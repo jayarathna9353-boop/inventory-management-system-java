b066392401d644e52f02167f89ba344ce8f8b4cb
jayarathna9353-boop <jayarathna9353@gmail.com>
2026-04-30 14:23:02 +0000

    Added inventory display

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Inventory Management System");
+        System.out.println("Product: Laptop");
+        System.out.println("Quantity: 10");
+        System.out.println("Price: 50000");
+    }
+}
