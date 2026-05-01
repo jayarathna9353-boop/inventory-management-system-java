d52989662295cddef2179ba9320879872e50ee32
jayarathna9353-boop <jayarathna9353@gmail.com>
2026-05-01 09:53:28 +0000

    Added multiple farm products

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         Product p1 = new Product("Rice Seeds", 50, 2500);
+        Product p2 = new Product("Fertilizer", 20, 1800);
+        Product p3 = new Product("Wheat Seeds", 30, 2000);
 
         System.out.println("Farmer Inventory Management System");
+
         p1.display();
+        System.out.println();
+
+        p2.display();
+        System.out.println();
+
+        p3.display();
     }
 }
