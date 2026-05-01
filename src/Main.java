51aec009d265c07182dee78059ac2fed0832c66d
jayarathna9353-boop <jayarathna9353@gmail.com>
2026-05-01 09:50:57 +0000

    Added Product class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Product p1 = new Product("Rice Seeds", 50, 2500);
+
         System.out.println("Farmer Inventory Management System");
-        System.out.println("Product: Rice Seeds");
-        System.out.println("Quantity: 50 Bags");
-        System.out.println("Price: 2500");
+        p1.display();
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -15,5 +15,3 @@ public class Product {
         System.out.println("Price: " + price);
     }
 }
-    
-
