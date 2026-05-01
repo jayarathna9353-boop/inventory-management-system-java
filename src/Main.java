f552344f6b6b688fd777820f19c3c5ee6ae98d65
jayarathna9353-boop <jayarathna9353@gmail.com>
2026-05-01 11:02:50 +0000

    Added looping menu system

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         Product p1 = new Product("Rice Seeds", 50, 2500);
 
-        System.out.println("Farmer Inventory Management System");
-        System.out.println("1. View Product");
-        System.out.println("2. Add Stock");
-        System.out.println("3. Sell Stock");
+        int choice;
 
-        System.out.print("Enter choice: ");
-        int choice = sc.nextInt();
+        do {
+            System.out.println("\nFarmer Inventory Management System");
+            System.out.println("1. View Product");
+            System.out.println("2. Add Stock");
+            System.out.println("3. Sell Stock");
+            System.out.println("4. Exit");
 
-        if(choice == 1) {
-            p1.display();
-        }
-        else if(choice == 2) {
-            p1.addStock(20);
-            p1.display();
-        }
-        else if(choice == 3) {
-            p1.sellStock(10);
-            p1.display();
-        }
+            System.out.print("Enter choice: ");
+            choice = sc.nextInt();
+
+            if(choice == 1) {
+                p1.display();
+            }
+            else if(choice == 2) {
+                p1.addStock(20);
+                p1.display();
+            }
+            else if(choice == 3) {
+                p1.sellStock(10);
+                p1.display();
+            }
+
+        } while(choice != 4);
 
         sc.close();
     }
