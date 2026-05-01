40acae47ef05f54eeec8d4a5ba5e03834afb710a
jayarathna9353-boop <jayarathna9353@gmail.com>
2026-05-01 11:00:25 +0000

    Added menu system

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         Product p1 = new Product("Rice Seeds", 50, 2500);
 
-        p1.display();
+        System.out.println("Farmer Inventory Management System");
+        System.out.println("1. View Product");
+        System.out.println("2. Add Stock");
+        System.out.println("3. Sell Stock");
 
-        p1.sellStock(35);
+        System.out.print("Enter choice: ");
+        int choice = sc.nextInt();
 
-        System.out.println("\nAfter Selling:");
-        p1.display();
+        if(choice == 1) {
+            p1.display();
+        }
+        else if(choice == 2) {
+            p1.addStock(20);
+            p1.display();
+        }
+        else if(choice == 3) {
+            p1.sellStock(10);
+            p1.display();
+        }
 
-        p1.checkLowStock();
+        sc.close();
     }
 }
